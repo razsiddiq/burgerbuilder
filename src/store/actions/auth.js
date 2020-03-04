af23dcb6b02398c02274c5f89cb6ad10c60f8af0
siddiq <siddiq.jalalu@gmail.com>
2020-03-04 19:57:05 +0530

    Logout updated

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -10,7 +10,7 @@ export const authStart = () =>{
 export const authSuccess = (idToken, userId) =>{
     return{
         type:actionTypes.AUTH_SUCCESS,
-        idToken:idToken,
+        token:idToken,
         userId :userId
     }
 }
@@ -20,6 +20,20 @@ export const authFail= (error) =>{
     return{
         type:actionTypes.AUTH_FAIL,
         error:error
+    }
+}
+
+export const logout = () =>{
+    return{
+        type:actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(()=> {
+            dispatch(logout());
+        }, expirationTime * 1000)
     }
 }
 
@@ -42,6 +56,7 @@ export const auth = (email, password, isSignup) =>{
             .then(response =>{
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken,response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn))
             })
             .catch(err => {
                 console.log(err);
